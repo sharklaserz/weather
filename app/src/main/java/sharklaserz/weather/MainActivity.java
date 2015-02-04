c706454f34c6e058e6e01e202ad3dbf287679908
Todd Silvia <todd.silvia@gmail.com>
2015-02-03 22:35:16 -0600

    Display response on screen

--- app/src/main/java/sharklaserz/weather/MainActivity.java
+++ app/src/main/java/sharklaserz/weather/MainActivity.java
@@ -2,18 +2,39 @@ package sharklaserz.weather;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.net.URL;
+
+import sharklaserz.weather.services.weatherapi.AsyncResponse;
+import sharklaserz.weather.services.weatherapi.httpRequest;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements AsyncResponse {
+
+    TextView txView;
+    httpRequest httpInstance = new httpRequest();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txView = (TextView) findViewById(R.id.putDataHere);
+        httpInstance.delegate = this;
     }
 
+    protected void onStart() {
+        super.onStart();
+        URL myUrl = null;
+        try {
+            myUrl = new URL("https://api.forecast.io/forecast/94c793f57c94d2ef0102edce1351d51d/37.8267,-122.423");
+        } catch (Exception e){}
+        httpInstance.execute(myUrl);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +56,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void processFinish(String output) {
+        txView.setText(output);
     }
 }
