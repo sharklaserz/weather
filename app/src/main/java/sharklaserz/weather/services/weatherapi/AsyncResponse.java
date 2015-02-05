83da91a7198ad9b3bb97e01b1239d7e727f0fc1e
Todd Silvia <todd.silvia@gmail.com>
2015-02-04 21:15:46 -0600

    Re-arranged to use event bus and weather api wrapper

--- app/src/main/java/sharklaserz/weather/services/weatherapi/AsyncResponse.java
+++
@@ -1,8 +0,0 @@
-package sharklaserz.weather.services.weatherapi;
-
-/**
- * Created by todds_000 on 2/3/2015.
- */
-public interface AsyncResponse {
-    void processFinish(String output);
-}
---
+++ app/src/main/java/sharklaserz/weather/services/weatherapi/WeatherAPI.java
@@ -0,0 +1,9 @@
+package sharklaserz.weather.services.weatherapi;
+
+/**
+ * Created by toddish on 2/4/2015.
+ */
+public interface WeatherAPI {
+
+    void getCurrentTemperature();
+}
