99a91dfa3aca6f8b7965e3a4955bf7d1f93d1940
Todd Silvia <todd.silvia@gmail.com>
2015-02-15 18:09:50 -0600

    Cannot get http to view

--- app/src/main/java/sharklaserz/weather/MainActivity.java
+++ app/src/main/java/sharklaserz/weather/MainActivity.java
@@ -1,27 +1,49 @@
 package sharklaserz.weather;
 
+import android.app.ActionBar;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.squareup.otto.Subscribe;
 
+import org.json.JSONObject;
+
+import nucleus.presenter.PresenterCreator;
+import nucleus.view.NucleusActivity;
+import nucleus.view.PresenterFinder;
+import sharklaserz.weather.loader.ForecastIOAPI;
+import sharklaserz.weather.presenter.MainPresenter;
 import sharklaserz.weather.services.weatherapi.WeatherWrapper;
 import sharklaserz.weather.tools.EventBus;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends NucleusActivity<MainPresenter> {
 
-    private TextView txView;
+    public TextView txView;
     String forecast = "FORECASTIO"; //Temporary, this should be moved into a file that manages constant strings.
+/*
+    @Override
+    protected PresenterCreator<MainPresenter> getPresenterCreator() {
+
+        return new PresenterCreator<MainPresenter>() {
+            @Override
+            public MainPresenter createPresenter() {
+                return new MainPresenter();
+            }
+        };
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.weather_home);
 
         //txView = (TextView) findViewById(R.id.putDataHere);
@@ -33,9 +55,10 @@ public class MainActivity extends ActionBarActivity {
 
     protected void onStart() {
         super.onStart();
-
-        WeatherWrapper myWrapper = new WeatherWrapper(forecast, getApplicationContext());
-        myWrapper.getCurrentTemperature();
+        ActionBar actionBar = getActionBar();
+        actionBar.show();
+        //WeatherWrapper myWrapper = new WeatherWrapper(forecast, getApplicationContext());
+        //myWrapper.getCurrentTemperature();
     }
 
     @Override
@@ -60,10 +83,113 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+*/
 
-    @Subscribe
-    public void handleMyEvent(String response) {
-        txView.setText(response);
+/*
+    private static final String PRESENTER_STATE_KEY = "presenter_state";
+
+    private MainPresenter presenter;
+
+    //@Override
+    public MainPresenter getPresenter() {
+        return presenter;
+    }
+
+    protected PresenterCreator<MainPresenter> getPresenterCreator() {
+        return new PresenterCreator<MainPresenter>() {
+            @Override
+            public MainPresenter createPresenter() {
+                return new MainPresenter();
+            }
+        };
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_home);
+        Bundle savedPresenterState = savedInstanceState == null ? null : savedInstanceState.getBundle(PRESENTER_STATE_KEY);
+        PresenterCreator<MainPresenter> creator = getPresenterCreator();
+        if (creator != null)
+            presenter = (MainPresenter) PresenterFinder.getInstance().findParentPresenter(this).provide(creator, savedPresenterState);
+
+        txView = (TextView) findViewById(R.id.dispTemp);
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        if (presenter != null)
+            presenter.takeView(this);
+    }
+
+    /**
+     * Presenter destruction is here because we want Activity's presenter to live longer than view's presenter
+     */
+
+/*
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+
+        if (presenter != null) {
+            presenter.dropView(this);
+
+            if (isFinishing()) {
+                presenter.destroy();
+                presenter = null;
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (presenter != null)
+            outState.putBundle(PRESENTER_STATE_KEY, presenter.save());
+
+    }
+
+    public void publishItems(JSONObject resp) {
+
+        txView = (TextView) findViewById(R.id.dispTemp);
+        if(resp != null) {
+            txView.setText(resp.toString());
+        }
+
+    }
+*/
+
+    static PresenterCreator<MainPresenter> presenterCreator = new PresenterCreator<MainPresenter>() {
+        @Override
+        public MainPresenter createPresenter() {
+            return new MainPresenter();
+        }
+    };
+
+    @Override
+    protected PresenterCreator<MainPresenter> getPresenterCreator() {
+        return presenterCreator;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_home);
+        txView = (TextView) findViewById(R.id.dispTemp);
+    }
+
+    public void publishItems(JSONObject theResponse) {
+
+        TextView myView = (TextView)findViewById(R.id.dispTemp);
+
+        if(theResponse == null) {
+            return;
+        }
+
+        myView.setText( theResponse.toString() );
     }
 
 }
